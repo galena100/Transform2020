dfd634beacec7596742a1e6420c35be4d0c9a165
leocd91 <leocd91@gmail.com>
2020-06-14 11:18:33 +0700

    Add files via upload

--- gr_bound.py
+++ gr_bound.py
@@ -8,7 +8,7 @@ def gr_bound(dep,curve,fscales,thres):
     #    curve : array_like, shape (N,)
     #        the 1d array value of GR curve
     #    fscales : array_like, shape (N,)
-    #        the 1d array scale of frequency used
+    #        the 1d array scale of frequency resolution (not the range frequency)
     #    thres : float
     #        threshold minimum contour samples that makes a contour eligible for boundary
     #        strength calculation. Its thres x standart deviation of number sample per contour
@@ -36,7 +36,7 @@ def gr_bound(dep,curve,fscales,thres):
     import pywt
     import matplotlib.pyplot as plt
     import numpy as np
-    
+    import pandas as pd
     wavelet = 'gaus2'
     
     dt=dep[1]-dep[0]
@@ -85,7 +85,8 @@ def gr_bound(dep,curve,fscales,thres):
             x0 = np.hstack((x0, dat0[:,1]))
             i0 = np.hstack((i0, np.ones([len(dat0[:,1])])*id0))
             id0=id0+1
-    
+    x0=x0-np.min(dep)
+    #x0[x0<0]=x0[x0<0]*-1.0
     #rescale to a array position scale (to call the zcwt2)
     x02=x0/dt
     y02=(y0-np.min(y0))/(np.max(y0)-np.min(y0))*(len(fscales)-1)
@@ -93,6 +94,7 @@ def gr_bound(dep,curve,fscales,thres):
     z0=np.zeros([x0.shape[0]],dtype=float)
     
     for i in range(z0.shape[0]):
+       # if int(np.floor(x02[i])) < (zcwt2.shape[0]):
         z0[i]=zcwt2[int(np.floor(x02[i])),int(np.floor(y02[i]))]
     
     #getting the boundary
@@ -113,7 +115,7 @@ def gr_bound(dep,curve,fscales,thres):
     
     bo=bo[bo[:,0].argsort()]
     
-    ax2.plot((bo[:,1]/fscales.shape[0])*(np.max(fscales)-np.min(fscales))+np.min(fscales),bo[:,0]*dt,'wo',markersize=5,markeredgecolor='y')        
+    ax2.plot((bo[:,1]/fscales.shape[0])*(np.max(fscales)-np.min(fscales))+np.min(fscales),(bo[:,0])*dt+np.min(dep),'wo',markersize=5,markeredgecolor='y')        
     ax2.yaxis.set_visible(False)
     
     #calculate the boundary strength & getting the signal polarity
@@ -122,10 +124,70 @@ def gr_bound(dep,curve,fscales,thres):
         bos[int(np.round(bo[i,0])),0]=np.abs(bo[i,2])/np.max(np.abs(bo[:,2]))
         bos[int(np.round(bo[i,0])),1]=np.sign(curve[int(np.round(bo[i,0]))])
     
-    ax3.barh(dep,bos[:,0])
-    ax3.set_ylim([np.min(dep),np.max(dep)])
-    ax3.invert_yaxis()
+    data=bos
+    mosaic=np.zeros([data.shape[0],100],dtype=int)*np.nan
+    
+    colnum=1000
+    colrange=np.linspace(0,colnum,data.shape[0],dtype=int)
+    
+    for i in range(0,data.shape[0]):
+        if data[i,0] != 0.0:
+            mosaic[i,0:int(np.floor(data[i,0]*100.0))]=colrange[i]
+    for i in range(0,data.shape[0]):
+        if ~np.isnan(mosaic[i,0]):
+            for j in range(0,100):
+                if np.isnan(mosaic[i,j]):
+                    fl=i
+                    while True:
+                        mosaic[fl,j-1]=mosaic[i,0]
+                        fl=fl-1
+                        if fl<0 or ~np.isnan(mosaic[fl,j-1]):
+                            break
+                    break
+    for i in range(0,data.shape[0]):
+        if ~np.isnan(mosaic[i,0]):
+            for j in range(0,100):
+                if np.isnan(mosaic[i,j]):
+                    fl=i+1
+                    while True:
+                        mosaic[fl,j-1]=mosaic[i,0]+2
+                        fl=fl+1
+                        if fl>data.shape[0]-1 or ~np.isnan(mosaic[fl,j-1]):
+                            break
+                    break
+    mosaic[0:np.where(data[:,0]==1.0)[0][0],99]=100.0
+    mosaic[np.where(data[:,0]==1.0)[0][0]:,99]=mosaic[np.where(data[:,0]==1.0)[0][0],0]
+    mosaic2=mosaic
+    pdmos=pd.DataFrame(mosaic)
+    pdmos=pdmos.T
+    pdmos=pdmos.fillna(method='bfill').T
+    mosaic=pdmos.values
+    
+    mosaic3=mosaic[np.arange(0,data.shape[0],10),:]
+    x=np.where(~np.isnan(mosaic2))[0][:]
+    x=np.floor(x/10).astype(int)
+    y=np.where(~np.isnan(mosaic2))[1][:]
+    grids=np.zeros([mosaic3.shape[0],100])*np.nan
+    grids[x,y]=1.0  
+    
+    #ax3.barh(dep,bos[:,0])
+    #ax3.set_ylim([np.min(dep),np.max(dep)])
+    #ax3.invert_yaxis()
+    #ax3.set_aspect('auto')
+    
+    
+    ax3.imshow(mosaic3,cmap='Spectral_r')
+    ax3.imshow(grids,interpolation='none')
     ax3.set_aspect('auto')
+    
+    ax1.set_ylabel('Depth (M)')
+    ax1.set_title('Dens Conditioned')
+    ax2.set_title('CWT Boundary Strength')
+    ax3.set_title('Mosaic')
+    
+    ax1.xaxis.set_visible(False)
+    ax2.xaxis.set_visible(False)
+    ax3.xaxis.set_visible(False)
     ax3.yaxis.set_visible(False)
-    return(bo,bos)
+    return(bo,bos,mosaic3,frequencies)
     
